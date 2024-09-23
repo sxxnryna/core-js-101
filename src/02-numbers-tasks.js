77156e1b36ac4335cb970839dd53e5fae6d06872
Ryna <katrin_dr@naver.com>
2024-09-23 23:39:14 +0900

    feat: do part 2

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -7,8 +7,8 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
+ * /**
  * Returns an area of a rectangle given by width and height.
  *
  * @param {number} width
@@ -19,12 +19,12 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
-
 /**
+ * /**
  * Returns a circumference of circle given by radius.
  *
  * @param {number} radius
@@ -35,11 +35,12 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
 
 /**
+ * /**
  * Returns an average of two given numbers.
  *
  * @param {number} value1
@@ -51,11 +52,12 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return (value1 + value2) / 2;
 }
 
 /**
+ * /**
  * Returns a distance between two points by cartesian coordinates.
  *
  * @param {number} x1
@@ -70,11 +72,14 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const deltaX = x2 - x1;
+  const deltaY = y2 - y1;
+  return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
 /**
+ * /**
  * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
  *
  * @param {number} a
@@ -86,12 +91,12 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
-
 /**
+ * /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
  * coordinates in Cartesian plane.
  * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
@@ -109,11 +114,17 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitudeA = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const magnitudeB = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  const cosineAngle = dotProduct / (magnitudeA * magnitudeB);
+  return Math.acos(cosineAngle);
 }
 
 /**
+ * /**
  * Returns a last digit of a integer number.
  *
  * @param {number} value
@@ -125,12 +136,12 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return Math.abs(value) % 10;
 }
 
-
 /**
+ * /**
  * Returns a number by given string representation.
  *
  * @param {string} value
@@ -141,12 +152,13 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
- * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
+ * /**
+ * Returns a diagonal length of the rectangular parallelepiped given by its sides a, b, c.
  *
  * @param {number} a
  * @param {number} b
@@ -158,12 +170,12 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
-
 /**
+ */**
  * Returns the number rounded to specified power of 10.
  *
  * @param {number} num
@@ -180,16 +192,19 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const factor = 10 ** pow;
+  return Math.round(num / factor) * factor;
 }
 
+
 /**
- * Returns true is the number is prime; otherwise false.
+ * /**
+ * Returns true if the number is prime; otherwise false.
  * See: https://en.wikipedia.org/wiki/Primality_test
  *
  * @param {number} n
- * @return {bool}
+ * @return {boolean}
  *
  * @example:
  *   4 => false
@@ -201,11 +216,21 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false; // 0 and 1 are not prime numbers
+  if (n <= 3) return true; // 2 and 3 are prime numbers
+  if (n % 2 === 0 || n % 3 === 0) return false; // eliminate multiples of 2 and 3
+
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+
+  return true;
 }
 
+
 /**
+ * /**
  * Tries to convert value to number and returns it if conversion was successful;
  * otherwise returns default value passed as a second argument.
  *
@@ -220,9 +245,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  return isNaN(num) ? def : num;
 }
+
 
 module.exports = {
   getRectangleArea,
